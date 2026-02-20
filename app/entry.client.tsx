d2e178125e45ccde09b6b99568bf179a0119ae7f
Karthikeyan Sundararajan <ksundararajan@nishtechinc.com>
2026-02-20 16:05:11 +0530

    corrections

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -2,10 +2,10 @@ import {RemixBrowser} from '@remix-run/react';
 import {startTransition, StrictMode} from 'react';
 import {hydrateRoot} from 'react-dom/client';
 
-if (!window.location.origin.includes('webcache.googleusercontent.com')) {
+if (typeof document !== 'undefined') {
   startTransition(() => {
     hydrateRoot(
-      document,
+      document.getElementById('root')!,
       <StrictMode>
         <RemixBrowser />
       </StrictMode>,
